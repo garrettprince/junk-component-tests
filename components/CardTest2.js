4dfbb8064a223a3478f6050e722b03b840847e10
garrettprince <89549339+garrettprince@users.noreply.github.com>
2022-07-17 16:29:53 -0700

    Formatting changes on Card 2

--- components/CardTest2.js
+++ components/CardTest2.js
@@ -9,9 +9,9 @@ function CardTest({ title, description, category }) {
     >
       <header className="flex justify-between items-center mx-4 mt-3">
         <h1 className="font-bold text-[1.70rem]">Do another thing</h1>
-        {/* <div className="border-[6px] bg-red-500/20 text-red-500 rounded-xl border-red-500 text-sm font-black px-2 py-0">
-          URGENT
-        </div> */}
+        <div className="border-[6px] bg-red-500/20 text-red-500 rounded-full border-red-500 text-sm font-black px-2 py-0">
+          !
+        </div>
       </header>
       <section id="description-text">
         <p className="my-3 mx-4 leading-[1.35rem] font-regular">
@@ -24,7 +24,7 @@ function CardTest({ title, description, category }) {
         </p> */}
       </section>
       <footer className="flex justify-between mx-4 mt-4 mb-1 content-center">
-        <h2 className="font-black text-green-500">DEV</h2>
+        <h2 className="font-black text-green-500">DESIGN</h2>
         <h2 className="font-black text-green-500">DUE 7/17</h2>
         <DotsHorizontalIcon className="h-8 w-8 cursor-pointer pb-[0.35rem] text-green-500" />
       </footer>
